c2c2d55b1d5c890c40a50dbb417bfefb2c4c6f0a
Alexandr Romaniv <alexandr.romaniv@mtrading.com>
2023-04-14 23:01:25 +0300

    add send form to telegram

--- src/js/import/form.js
+++ src/js/import/form.js
@@ -1,9 +1,13 @@
 import toastr from "toastr";
+import axios from "axios";
 
 const form = document.querySelector(".form");
 toastr.options.positionClass = "toast-top-right";
 toastr.options.progressBar = true;
-form.addEventListener("submit", async (e) => {
+
+const TG_TOKEN = "5871941331:AAFdm_WlnfgXaRibgrbq8mE4olz2fTXRGaA";
+const CHAT_ID ="-1001973921078";
+form.addEventListener("submit", async function  (e) {
     const modal = document.querySelector("[data-modal]");
     e.preventDefault(); // Отменяем стандартное поведение отправки формы
 
@@ -15,6 +19,16 @@ form.addEventListener("submit", async (e) => {
             "Accept": "application/json"
         }
     });
+    //     send to telegram
+
+
+    const name = form.elements.name.value;
+    const phone = form.elements.number.value;
+    const email = form.elements.email.value;
+    const message = form.elements.comment.value;
+
+    const url = `https://api.telegram.org/bot${TG_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=Имя: ${name}%0AТелефон: ${phone}%0AПочта: ${email}%0AСообщение: ${message}`;
+    await axios.post(url);
 
     if (response.ok) {
         form.reset(); // Очищаем значения полей формы
@@ -23,4 +37,6 @@ form.addEventListener("submit", async (e) => {
     } else {
         toastr.error("Повідомлення не вдалося надіслати!");
     }
+
+
 });
