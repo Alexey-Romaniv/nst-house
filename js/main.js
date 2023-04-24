ffa8d0b81538563fb008e5911e0d8a56aa741e4a
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-04-24 13:49:50 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -187,14 +187,14 @@ if (form) {
 
 (function () {
   var refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]')
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]")
   };
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
   function toggleModal() {
-    refs.modal.classList.toggle('is-open');
+    refs.modal.classList.toggle("is-open");
   }
 })();
 
